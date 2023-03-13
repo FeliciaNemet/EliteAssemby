3d29687900f7d3c73c6b5230f2487584b5d9b4ee
Felicia Nemet <87214743+FeliciaNemet@users.noreply.github.com>
2023-03-13 05:03:30 -0400

    somethings are beginning to work

--- src/App.js
+++ src/App.js
@@ -3,40 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          L💡te Lessons
-          {/* Lite Lessons <code>src/App.js</code> and save to reload. */}
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/lite_rails/?hl=en"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @Lite_Rails
-        </a>
-        {/* <form action="" onSubmit={this.handleSubmit}>
-					<legend>Do you prefer light or dark?</legend>
-					<label for="pie">pie</label>
-					<input type="radio" id="pie" value="pie" name="size" />
-			
-					<label for="slice">slice</label>
-					<input type="radio" id="slice" value="slice" name="size" />
-
-				<fieldset class="choices">
-					<legend>Do you prefer Hot <span class="crystal">🥵</span> or Freezing <span class="crystal">🥶</span>?</legend>
-					<label for="$">a little</label>
-					<input type="radio" name="price" id="$" value="$" />
-			
-					<label for="$$">a little more</label>
-					<input type="radio" name="price" id="$$" value="$$"/>
-				</fieldset>
-			
-				<input type="submit" value="En-L💡ghten Yourself!!"/> 
-			</form> */}
-      <p>En-l💡ghten Yourself!!</p>
-      </header>
+      
     </div>
   );
 }
