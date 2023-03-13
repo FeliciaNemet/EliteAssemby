0aa382d3b3487c61495d680dd8783918f7f5c789
Felicia Nemet <87214743+FeliciaNemet@users.noreply.github.com>
2023-03-13 03:47:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,39 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          L💡te Lessons
+          {/* Lite Lessons <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/lite_rails/?hl=en"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          @Lite_Rails
         </a>
+        {/* <form action="" onSubmit={this.handleSubmit}>
+					<legend>Do you prefer light or dark?</legend>
+					<label for="pie">pie</label>
+					<input type="radio" id="pie" value="pie" name="size" />
+			
+					<label for="slice">slice</label>
+					<input type="radio" id="slice" value="slice" name="size" />
+
+				<fieldset class="choices">
+					<legend>Do you prefer Hot <span class="crystal">🥵</span> or Freezing <span class="crystal">🥶</span>?</legend>
+					<label for="$">a little</label>
+					<input type="radio" name="price" id="$" value="$" />
+			
+					<label for="$$">a little more</label>
+					<input type="radio" name="price" id="$$" value="$$"/>
+				</fieldset>
+			
+				<input type="submit" value="En-L💡ghten Yourself!!"/> 
+			</form> */}
+      <p>En-l💡ghten Yourself!!</p>
       </header>
     </div>
   );
